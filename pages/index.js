03a4b0c2c88132d7c0cf138c8cec5c20f79aad1e
EdithGdev <edithgonzalez9630@gmail.com>
2022-03-26 12:00:57 -0700

    setup complete

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 
 export default function Home() {
   return (
-    // get testnet connected
-<h1>yeahhh</h1>
+<Header />
   )
 }
+
+
