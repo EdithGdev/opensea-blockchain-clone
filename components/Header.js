f307321af33b53d3927188b8c1b5df2f492e23f8
EdithGdev <edithgonzalez9630@gmail.com>
2022-03-30 11:49:43 -0700

    Image component not defined

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+import openseaLogo from '../assets/opensea.png'
 
 const style = {
 wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
@@ -14,7 +16,14 @@ headerIcon: `text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-poi
 
 const Header = () => {
 return (
-    <div>HEADER</div>
+    <div className={style.wrapper}>
+    <Link href='/'>
+        <div className={style.logoContainer}>
+            <Image src={openseaLogo} height={40} width={40} />
+        </div>
+    </Link>
+
+    </div>
 )
 }
 
